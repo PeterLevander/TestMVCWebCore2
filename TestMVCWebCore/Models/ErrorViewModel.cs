16e7e76b0143fe078f3d7be717ba5394db94300f
PeterLevander <peter.levander@evry.com>
2019-12-03 14:11:57 +0100

    Add project files.

---
+++ TestMVCWebCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMVCWebCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
