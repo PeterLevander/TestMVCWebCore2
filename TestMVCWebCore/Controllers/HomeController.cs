4d7a4d3e40bdc216d202115646aa0fd349e61027
PeterLevander <peter.levander@evry.com>
2019-12-03 17:55:11 +0100

    3. Appsettings

--- TestMVCWebCore/Controllers/HomeController.cs
+++ TestMVCWebCore/Controllers/HomeController.cs
@@ -4,12 +4,19 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using TestMVCWebCore.Models;
 
 namespace TestMVCWebCore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration )
+        {
+            this.configuration = configuration;
+        }
         public IActionResult Index()
         {
             return View();
@@ -17,9 +24,10 @@ namespace TestMVCWebCore.Controllers
 
         public IActionResult About()
         {
+            var model = configuration["Greeting"];
             ViewData["Message"] = "Your application description page.";
 
-            return View();
+            return View("About", model);
         }
 
         public IActionResult Contact()
