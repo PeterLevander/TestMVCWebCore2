548978735cc1afeab457e6cc1335ec779a3a4d6a
PeterLevander <peter.levander@evry.com>
2019-12-05 16:51:23 +0100

    Connect to extern DB

--- TestMVCWebCore/Data/Migrations/20191204161031_initial.Designer.cs
+++ TestMVCWebCore/Data/Migrations/20191204161031_initial.Designer.cs
@@ -11,9 +11,10 @@ using TestMVCWebCore.Data;
 namespace TestMVCWebCore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191204161031_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TestMVCWebCore/Program.cs
+++ TestMVCWebCore/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using TestMVCWebCore.Data;
 
 namespace TestMVCWebCore
 {
@@ -14,7 +17,19 @@ namespace TestMVCWebCore
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            MigrateDatabase(host);
+            host.Run();
+
+        }
+        public static void MigrateDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
